f6bbbaa32e6973d182f98757d143b3ee0ff72311
Vy <vttvy.20it4@vku.udn.vn>
2025-04-10 21:38:43 +0700

    initial project

---
+++ src/main/java/com/project/ecommerce_services/EcommerceServicesApplication.java
@@ -0,0 +1,13 @@
+package com.project.ecommerce_services;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceServicesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/ecommerce_services/EcommerceServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.ecommerce_services;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
