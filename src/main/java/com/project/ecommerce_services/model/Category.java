d73999411a82a4cf5ee671908f1a52febc6a4a41
Vy <vttvy.20it4@vku.udn.vn>
2025-04-11 00:14:34 +0700

    feat-1: CRUD category

---
+++ src/main/java/com/project/ecommerce_services/model/Category.java
@@ -0,0 +1,26 @@
+package com.project.ecommerce_services.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity(name = "categories")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Category {
+	
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long categoryId;
+
+    @NotBlank
+    @Size(min = 5, message = "Category name must be at least 5 characters long")
+    private String categoryName;
+}
---
+++ src/main/java/com/project/ecommerce_services/payload/CategoryResponse.java
@@ -0,0 +1,19 @@
+package com.project.ecommerce_services.payload;
+
+import java.util.List;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class CategoryResponse {
+	private List<CategoryDTO> content;
+    private Integer pageNumber;
+    private Integer pageSize;
+    private Long totalElements;
+    private Integer totalpages;
+    private boolean lastPage;
+}
